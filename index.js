bb8c802772f4124a79a4124d07762b420bea1c02
thedudefreebird <tyler.freebery@gmail.com>
2020-01-11 19:07:50 -0500

    using new url for extracting page search

--- index.js
+++ index.js
@@ -3,34 +3,56 @@
 const apiKey = '<your api token here>';
 const searchURL = "https://cors-anywhere.herokuapp.com/http://en.wikipedia.org/w/api.php?";
 
+//Function to create url for fetch
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${key}=${params[key]}`);
     return queryItems.join('&');
 }
 
+//Function to display what is given from fetch
 function displayResults(responseJson) {
   console.log(responseJson);
   $('#results-list').empty();
 
-  for(let i = 0; i < responseJson.query.search.length; i++){
+  for(let i = 0; i < responseJson.query.pages.length; i++){
     $('#results-list').append(
       `
-      <li><h3>${responseJson.query.search[i].title}</h3></li>
+      <li><h3>${responseJson.query.pages[i].title}</h3></li>
       <ul>
-        <li>${responseJson.query.search[i].pageid}</li>
-        <li>${responseJson.query.search[i].snippet}</li>
+        <li>${responseJson.query.pages[i].pageid}</li>
+        <li>${responseJson.query.pages[i].extract}</li>
       </ul>
       `);}
   $('#results').removeClass('hidden');
+
+//Postman path:
+//query -> pages -> pageid(just in numerical form, not specifying pageid) -> pageid, ns, title, extract.
 }
 
-function getBands(genre, maxResults=10) {
+//Function to fetch info from URL
+function getBands(genre, maxResults=5) {
   const params = {
-    "action": "query",
-    "list": "search",
-    srsearch: genre,
     "format": "json",
+    "action": "query",
+    "prop": "extracts&exintro&explaintext",
+    // "exintro": ``,//No = needed for param, with uncommented, below occurs
+    // "explaintext": ``,//No = needed for param, with uncommented, below occurs
+    "redirects": "1",
+    "titles": genre,
+
+    //prop=extracts&exintro&explaintext&redirects=1&titles=
+    //
+    //Above should be url to fetch from
+    //
+    //prop=extracts&exintro=&explaintext=&redirects=1&titles=
+    //Above is current issue
+
+    //Working params for search of just quick display from wiki:
+    // "action": "query",
+    // "list": "search",
+    // srsearch: genre,
+    // "format": "json",
   };
   const queryString = formatQueryParams(params)
   const url = searchURL + queryString;
@@ -49,6 +71,7 @@ function getBands(genre, maxResults=10) {
     });
 }
 
+//Watchform Function
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
